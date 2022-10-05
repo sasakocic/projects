4a8bcab6768f84d60b055ddbb0b7c0b6a116b010
Sasa Kocic <sasa.kocic@gmail.com>
2022-10-05 14:48:19 +0200

    rails generate scaffold task name description:text project:references member:references status

---
+++ db/migrate/20221005124606_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.references :project, null: false, foreign_key: true
+      t.references :member, null: false, foreign_key: true
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
